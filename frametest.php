358098b4d8ddcc79aadb05b9dc88bf9b2d6de1b0
RNLabs <silver2prod@gmail.com>
2020-03-23 00:10:32 +0500

    OMG Danila
    I am crazy :+1:
    Test runs on local

--- frametest.php
+++ frametest.php
@@ -1,3 +1,8 @@
+<?
+$link = $_GET['link'];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +10,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="./front/js/frame.js"></script>
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.5.10/plyr.css" />
     <link rel="stylesheet" href="./front/css/frame.css">
 </head>
 <body>
-    <iframe id='main_frame' src="http://theproject.me/api/api.php?action=getiframe&embed_link=https://www.pornhub.com/embed/ph568908a4ca079" frameborder="0"></iframe>
+    <iframe id='main_frame' src="http://theproject.me/api/api.php?action=getiframe&embed_link=<? echo urlencode($link); ?>" frameborder="0"></iframe>
+        <video id = "main_player" poster="<? echo $thumbnail; ?>" playsinline controls>
+			<source id="main_player_source" src="" type="video/mp4" />
+		</video>
+
+
+
+
+
+        
+        <script src="https://cdn.plyr.io/3.5.10/plyr.js"></script>
+        <script src="./front/js/frame.js"></script>
 </body>
 </html>
--- front/js/frame.js
+++ front/js/frame.js
@@ -5,6 +5,10 @@ setTimeout(() => {
 
     //var link = $('#main_frame').contents().find('source')[0];
     var vars = document.getElementById("main_frame").contentWindow.flashvars;
-    console.log(vars.mediaDefinitions[0].videoUrl);
-
+    var link = vars.mediaDefinitions[0].videoUrl;
+    console.log(link);
+    $('#main_player_source').attr('src',link);
+    const player = new Plyr('#main_player');
+    
 }, 1000); 
+
